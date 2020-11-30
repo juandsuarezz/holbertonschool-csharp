f1345f2b09f6cd217ccb4ef3fa5d4a2a151680af
juandsuarezz <1486@holbertonschool.com>
2020-11-30 15:36:44 -0500

    Add concat

--- 0x00-csharp-hello_world/7-concat/7-concat.cs
+++ 0x00-csharp-hello_world/7-concat/7-concat.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace _7_concat
+class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	static void Main(string[] args)
+	{
+		string str1 = "Holberton";
+		string str2 = "School";
+		str1 = str1 + " " + str2;
+		Console.WriteLine("Welcome to {0}!", str1);
+	}
 }
