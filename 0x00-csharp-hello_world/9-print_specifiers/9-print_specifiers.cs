e9270f32a9aafa366d3fc4bd652180f5333e34ba
juandsuarezz <1486@holbertonschool.com>
2020-11-30 16:32:46 -0500

    Add specifiers

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,6 +6,6 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine($"Percent: {percent.ToString("P2", System.Globalization.CultureInfo.InvariantCulture)}\nCurrency: {currency.ToString("C", System.Globalization.CultureInfo.CurrentCulture)}");
+		Console.WriteLine($"Percent: {percent.ToString("P2", System.Globalization.CultureInfo.CreateSpecificCulture("en-US"))}\nCurrency: {currency.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("en-US"))}");
 	}
 }
