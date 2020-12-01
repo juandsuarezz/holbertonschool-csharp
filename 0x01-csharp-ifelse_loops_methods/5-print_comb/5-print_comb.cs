a87ebff5718b96aa237c7115fbc9a96702d5acf7
juandsuarezz <1486@holbertonschool.com>
2020-12-01 09:38:52 -0500

    Add 0 to 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for ((int i, int max) = (0, 100); i < max; i++)
+        {
+            Console.Write("{0:D2}{1}", i, (i < max - 1 ? ", " : "\n"));
+        }
+    }
+}
