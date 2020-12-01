239e9ba254bbe14931c977f8359958ef63bb3cc2
juandsuarezz <1486@holbertonschool.com>
2020-12-01 09:05:56 -0500

    Add alphabet

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 'a'; i <= 'z'; i++)
+        {
+            Console.Write((char)i);
+        }
+    }
+}
