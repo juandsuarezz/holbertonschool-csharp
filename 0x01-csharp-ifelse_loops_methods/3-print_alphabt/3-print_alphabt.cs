a2afb25cc64cb4cfd12f59f6234ecb320dda9c1f
juandsuarezz <1486@holbertonschool.com>
2020-12-01 09:19:53 -0500

    Add alphabt

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 'a'; i <= 'z'; i++)
+        {
+            if (!(i == 'q' || i == 'e')) {
+            Console.Write((char)i);
+            }
+        }
+    }
+}
