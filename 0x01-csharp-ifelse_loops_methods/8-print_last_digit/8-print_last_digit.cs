2b3c66399036e685c48185a0ac8ee22af0420d8d
juandsuarezz <1486@holbertonschool.com>
2020-12-01 10:22:39 -0500

    Add lastdigit

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Number 
+{
+    public static int PrintLastDigit(int number) 
+    {
+        int n = Math.Abs(number % 10);
+        Console.Write(n);
+        return n;
+    }
+}
