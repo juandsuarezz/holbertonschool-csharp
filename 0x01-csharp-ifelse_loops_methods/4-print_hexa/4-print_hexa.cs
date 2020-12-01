0db7d622e22006507d497713624302dc6d6ab738
juandsuarezz <1486@holbertonschool.com>
2020-12-01 09:23:40 -0500

    Add hexadecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for (int i = 0; i <= 98; i++) {
+               Console.Write("{0} = 0x{0:x}\n", i);
+           }
+        }
+    }
