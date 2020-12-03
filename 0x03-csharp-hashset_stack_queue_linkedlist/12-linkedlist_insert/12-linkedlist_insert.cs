4dc42300133fccc265cf7e86f2b87f760e521881
juandsuarezz <1486@holbertonschool.com>
2020-12-03 17:28:52 -0500

    Add linkedlist insert

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> node = myLList.First;
+
+        if (n < node.Value)
+        {
+            return myLList.AddBefore(node, n);
+        }
+
+        while (node != null)
+        {
+            if (n >= node.Value && (node.Next == null || n < node.Next.Value))
+            {
+                return myLList.AddAfter(node, n);
+            }
+            node = node.Next;
+        }
+        return null;
+    }
+}
