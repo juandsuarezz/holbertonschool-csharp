4535bfdaa3337e1b90c75d2e91f3346dc992945b
juandsuarezz <1486@holbertonschool.com>
2020-12-03 17:24:09 -0500

    Add uniq add

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int total = 0;
+        HashSet<int> nums = new HashSet<int>();
+
+        foreach (int val in myList)
+        {
+            if (!nums.Contains(val))
+            {
+                total = total + val;
+                nums.Add(val);
+            }
+        }
+        return total;
+    }
+}
