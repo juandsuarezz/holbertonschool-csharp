414afbe76c6a2a738d4747a58402098730760732
juandsuarezz <1486@holbertonschool.com>
2020-12-03 17:26:10 -0500

    Add queue en de

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        if (aQueue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        }
+        bool foundSearch = aQueue.Contains(search);
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, foundSearch);
+
+        if (foundSearch)
+        {
+            string val;
+            do { 
+                val = aQueue.Dequeue();
+            }
+            while (val != search);
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
