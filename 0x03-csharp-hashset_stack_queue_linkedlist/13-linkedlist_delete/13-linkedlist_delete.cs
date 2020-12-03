dfd70d5f9a5038816178e890eab4667d3d865215
juandsuarezz <1486@holbertonschool.com>
2020-12-03 17:29:11 -0500

    Add linkedlist del

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> node = myLList.First;
+        int i = 0;
+
+        while (node != null)
+        {
+            if (i == index)
+            {
+                myLList.Remove(node);
+            }
+            node = node.Next;
+            i++;
+        }
+    }
+}
