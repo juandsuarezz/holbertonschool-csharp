0db4a6a7433ffd45969fbb79ea74a337545f867e
juandsuarezz <1486@holbertonschool.com>
2020-12-03 17:26:38 -0500

    Add linkedlist

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        size = size < 0 ? 0 : size;
+        LinkedList<int> newList = new LinkedList<int>(Enumerable.Range(0, size));
+
+        foreach (int val in newList)
+        {
+            Console.WriteLine(val);
+        }
+        return newList;
+    }
+}
