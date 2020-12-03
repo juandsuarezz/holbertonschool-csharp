ecb4d28fcf8c1bf45075b2221f2125696a834c83
juandsuarezz <1486@holbertonschool.com>
2020-12-03 17:24:40 -0500

    Add common element

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> common = new List<int>();
+
+        foreach (int val in list1)
+        {
+            if (list2.Contains(val))
+            {
+                common.Add(val);
+            }
+        }
+        return common;
+    }
+}
