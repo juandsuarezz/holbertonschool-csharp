73dea2c084b461461902b4299c9be14853da3c69
juandsuarezz <1486@holbertonschool.com>
2020-12-03 17:27:41 -0500

    Add linkedlist find

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int onIndex = 0;
+
+        foreach (int val in myLList)
+        {
+            if (val == value)
+            {
+                return onIndex;
+            }
+
+            onIndex++;
+        }
+        return -1;
+    }
+}
