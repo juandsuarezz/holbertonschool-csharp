927170953fb137f7c17ae37adf308f85d9e68681
juandsuarezz <1486@holbertonschool.com>
2020-12-03 17:25:30 -0500

    Add stack pop

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        }
+        bool foundSearch = aStack.Contains(search);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, foundSearch);
+
+        if (foundSearch)
+        {
+            string val;
+            do { 
+                val = aStack.Pop();
+            }
+            while (val != search);
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
