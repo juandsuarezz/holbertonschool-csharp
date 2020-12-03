cd163ed57b73a9881252286cec38501c18e779d7
juandsuarezz <1486@holbertonschool.com>
2020-12-03 17:28:35 -0500

    Add linkedlist sum

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+
+        foreach (int val in myLList)
+        {
+            sum += val;
+        }
+        return sum;
+    }
+}
