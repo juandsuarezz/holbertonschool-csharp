99bc124257412fe342ec542169a8f88646c1e6b4
juandsuarezz <1486@holbertonschool.com>
2020-12-02 21:09:12 -0500

    add advanced task

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] jaggedArray = new int[3][];
+
+        jaggedArray[0] = Enumerable.Range(0, 4).ToArray();
+        jaggedArray[1] = Enumerable.Range(0, 7).ToArray();
+        jaggedArray[2] = Enumerable.Range(0, 2).ToArray();
+
+        for (int i = 0; i < jaggedArray.Length; i++)
+        {
+            for (int j = 0; j < jaggedArray[i].Length; j++)
+            {
+                if (j == 0)
+                {
+                    Console.Write(jaggedArray[i][j]);
+                }
+                else
+                {
+                    Console.Write($" {jaggedArray[i][j]}");
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
