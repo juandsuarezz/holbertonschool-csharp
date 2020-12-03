d1db684d942f731fc9101049e865f7515842b10c
juandsuarezz <1486@holbertonschool.com>
2020-12-03 17:25:00 -0500

    Add dif elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> uncommon = new List<int>();
+
+        foreach (int val in list1)
+        {
+            if (!list2.Contains(val))
+            {
+                uncommon.Add(val);
+            }
+        }
+        foreach (int val in list2)
+        {
+            if (!list1.Contains(val))
+            {
+                uncommon.Add(val);
+            }
+        }
+        uncommon.Sort();
+        return uncommon;
+    }
+}
