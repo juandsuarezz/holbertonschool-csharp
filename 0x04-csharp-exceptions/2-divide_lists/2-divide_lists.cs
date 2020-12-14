d2aff546e570df697ce4c782405a5c385fe977f3
juandsuarezz <1486@holbertonschool.com>
2020-12-14 13:07:07 -0500

    Add divide list

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> quotients = new List<int>();
+
+        for (int i = 0; i < listLength; i++)
+        {
+            try
+            {
+                quotients.Add(list1[i] / list2[i]);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                quotients.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+
+        return quotients;
+    }
+}
