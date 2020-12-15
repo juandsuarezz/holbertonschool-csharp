3eebedeba6e9e39ffb3e82572909e097c9196aeb
juandsuarezz <1486@holbertonschool.com>
2020-12-15 13:01:13 -0500

    Add 2 enemy

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>A simple Zombie class</summary>
+    public class Zombie
+    {
+        /// <summary>The health of the Zombie</summary>
+        public int health;
+
+        /// <summary>Constructor for the Zombie</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>Constructor for the Zombie</summary>
+        /// <param name="value">Health</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+
+            this.health = value;
+        }
+    }
+}
