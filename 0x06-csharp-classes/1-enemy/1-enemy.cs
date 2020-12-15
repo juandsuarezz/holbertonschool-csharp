a7ba91dd1da322bae1bad82e8788f0fff9f08d71
juandsuarezz <1486@holbertonschool.com>
2020-12-15 12:48:55 -0500

    Add 1 enemy

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>A simple Zombie class</summary>
+    public class Zombie
+    {
+        /// <summary>The health of the Zombie</summary>
+        public int health;
+
+        /// <summary>Constructor for the Zombie</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
