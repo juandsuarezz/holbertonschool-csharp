3046fd108474dc14afdf05813bd3de53d59ab296
juandsuarezz <1486@holbertonschool.com>
2020-12-15 11:28:05 -0500

    Add 3 dog

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string n, float a, string o, Rating r)
+    {
+        name = n;
+        age = a;
+        owner = o;
+        rating = r;
+    }
+
+    public override string ToString()
+    {
+        return $"Dog Name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}";
+    }
+}
