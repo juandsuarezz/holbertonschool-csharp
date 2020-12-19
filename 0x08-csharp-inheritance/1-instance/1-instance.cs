03fc8999ebbf3f5bf0df889ae4ab08cb286ac202
juandsuarezz <1486@holbertonschool.com>
2020-12-19 14:50:54 -0500

    Add 1 instance

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>Class to conatin object methods</summary>
+class Obj
+{
+    /// <summary>Gets if an object is an instance of Array</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return (obj is Array);
+    }
+}
