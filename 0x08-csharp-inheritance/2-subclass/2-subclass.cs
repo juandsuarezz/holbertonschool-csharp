9f494070a7d42ded52e02198348841dd27b5c2f8
juandsuarezz <1486@holbertonschool.com>
2020-12-19 14:59:58 -0500

    Add 2 subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>Class for methods on objects</summary>
+class Obj
+{
+    /// <summary>Checks if a class is a subclass of another class</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return (derivedType.IsSubclassOf(baseType));
+    }
+}
