161bba65fb1bae2df3bb34a5d2696a686e761a55
juandsuarezz <1486@holbertonschool.com>
2020-12-19 15:03:17 -0500

    Add 3 type get

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+/// <summary>Class for object methods</summary>
+class Obj
+{
+    /// <summary>Prints names of properties and methods of an object</summary>
+    public static void Print(object myObj)
+    {
+        Type t = myObj.GetType();
+
+        Console.WriteLine($"{t.Name} Properties:");
+        foreach (PropertyInfo p in t.GetProperties())
+        {
+            Console.WriteLine(p.Name);
+        }
+
+        Console.WriteLine($"{t.Name} Methods:");
+        foreach (MethodInfo m in t.GetMethods())
+        {
+            Console.WriteLine(m.Name);
+        }
+    }
+}
