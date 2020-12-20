2d3d2a2a4e44a1f8a9b8838d963e445f6630fa65
juandsuarezz <1486@holbertonschool.com>
2020-12-20 14:57:33 -0500

    Add 6 shape

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>Base class for shapes</summary>
+class Shape
+{
+    /// <summary>Method to be overriden by children to get the area</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get { return this.width; }
+        set {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            
+            this.width = value;
+        }
+    }
+
+    public int Height
+    {
+        get { return this.height; }
+        set {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            
+            this.height = value;
+        }
+    }
+}
